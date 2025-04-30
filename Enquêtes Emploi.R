157ad74ebcd0e3b88025f3bed3f4e46fa084da68
l4hiru <saranath.rimlinger@gmail.com>
2025-04-30 14:25:04 +0200

    1974 variables and dep dataset cleaned

--- Enquêtes Emploi.R
+++ Enquêtes Emploi.R
@@ -19,91 +19,34 @@ data_1974 <- read_dta("1974/ee74.dta")
 
 #II) Variables --------------------------------------------------------------------
 
-freq(data_1974$s74) # Rural (0 + 1)
-
 data_1974 <- data_1974 %>%
   mutate(
-    Rural = ifelse(s74 %in% c("0", "1"), 1, 0))
-
-freq(data_1974$Rural)
-
-freq(data_1974$ad1) # Young (18 to 30)
-
-data_1974 <- data_1974 %>%
-  mutate(
-    Young = if_else(ad1 %in% sprintf("%02d", 18:30), 1, 0)
-  )
-
-freq(data_1974$Young)
-
-data_1974$Age <- as.numeric(data_1974$ad1)
-
-freq(data_1974$et)
-
-data_1974 <- data_1974 %>%
-  mutate(
-    Study = as.factor(et))
-
-freq(data_1974$et)
-
-freq(data_1974$eg) # General Diploma
-freq(data_1974$ep) # Profesionnal Diploma
-
-data_1974 <- data_1974 %>%
-  mutate(
+    Age = as.numeric(ad1),
+    Study = as.factor(et),
+    Rural = ifelse(s74 %in% c("0", "1"), 1, 0),
     Diploma = case_when(
-      # High: baccalauréat ou supérieur (général) OU BTS/DUT/université/etc. (pro)
       eg %in% c("3", "4", "5", "6", "7", "8") | ep %in% c("6", "7", "8", "9") ~ "High",
-      
-      # Medium: BEPC (général) ou CAP/BEP/etc. (pro)
       eg == "2" | ep %in% c("1", "2", "3", "4", "5") ~ "Medium",
-      
-      # Low: aucun diplôme ou non déclaré ou inférieur au BEPC
       TRUE ~ "Low"
     ),
-    Diploma = factor(Diploma, levels = c("Low", "Medium", "High"))
-  )
-
-freq(data_1974$Diploma) 
-
-freq(data_1974$taec) # 1 : Active and # 2 : Unemployed
-
-data_1974 <- data_1974 %>%
-  mutate(
+    Diploma = factor(Diploma, levels = c("Low", "Medium", "High")),
     Active = ifelse(taec == "1", 1, 0),
-    Unemployed = ifelse(taec == "2", 1, 0)
-  )
-
-freq(data_1974$Active)
-freq(data_1974$Unemployed)
-
-wtd.table(data_1974$Unemployed, weights = data_1974$redechi) # Weighting thanks to questionr package
-prop.table(wtd.table(data_1974$Unemployed, weights = data_1974$redechi)) # Weighted %
-
-freq(data_1974$cse1) # Occupation
-
-data_1974 <- data_1974 %>%
-  mutate(
+    Unemployed = ifelse(taec == "2", 1, 0),
     Occupation = case_when(
-      cse1 %in% c("00", "10") ~ "Farmers", # + 10 : Workers in agriculture
+      cse1 %in% c("00", "10") ~ "Farmers",
       cse1 %in% c("20") ~ "Craftsmen",
       cse1 %in% c("41", "42", "43", "44") ~ "PI",
       cse1 %in% c("30", "32", "33", "34") ~ "Executive",
-      cse1 %in% c("51", "53", "73") ~ "Employees", # + personnes en service
+      cse1 %in% c("51", "53", "73") ~ "Employees",
       cse1 %in% c("60", "61", "63", "65", "66", "67", "68") ~ "Workers",
-      cse1 %in% c("83", "90", "91", "92", "96", "97", "98", "99") ~ "Inactive", # 83: Others categories
+      cse1 %in% c("83", "90", "91", "92", "96", "97", "98", "99") ~ "Inactive",
       TRUE ~ NA_character_
     )
   )
 
-freq(data_1974$Occupation)
+freq(data_1974$redechi) # Weighting variable
 
-freq(data_1974$d) # Metropolitans Departments
-
-freq(data_1974$redechi) # Weighting variable ?
-
-
-freq(data_1974$bcnd)
+#III) Departemental dataset
 
 dep_1974 <- data_1974 %>%
   group_by(d) %>%
@@ -128,17 +71,4 @@ dep_1974 <- data_1974 %>%
   ) %>%
   ungroup()
 
-mean(dep_1974$YoungShare)
-
-freq(data_1974$redechi)
-
-table(data_1974$d, data_1974$UnemploymentRate)
-
-freq(data_1974$Occupation)
-freq(data_1974$Active)
-
-freq(data_1974$Age)
-
-detach("package:questionr", unload = TRUE)
-
-mean(dep_1974$UnemploymentRate)
+write_parquet(dep_1974, "dep_1974.parquet")
